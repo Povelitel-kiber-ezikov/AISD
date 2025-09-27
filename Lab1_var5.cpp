50911fcd3124f363e17df067a0a2d8e5b711c084
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2025-09-27 17:22:26 +0400

    Добавил метод перевыделения памяти

--- Lab1_var5.cpp
+++ Lab1_var5.cpp
@@ -2,13 +2,46 @@
 
 template <typename T>
 struct Point{
-	T x;
-	T y;
+private:
+	T x, y;
+
+public:
+
+	Point() : x(0), y(0) {}; 
+	Point(T x, T y) : x(x), y(y) {}; 
+
 };
 
 
 template <typename T>
 class Polyline {
-	Point<T> _points;
+private:
+	Point<T>* _points;
+	int  _size;
+	int _capacity;
+	
+
+	void resize(int new_capacity) {
+		if (new_capacity <= capacity) {
+			return;
+		}
+		Point<T>* new_point = new Point<T>[new_capacity];
+		for (int i = 0; i < size; ++i) {
+			new_points[i] = _points[i];
+			delete[] _points;
+			
+			_points = new_point;
+			_capacity = new_capacity;
+		}
+	}
+
+
+public:
+
 
 };
+
+
+int main() {
+	Polyline<int> p1;
+}
